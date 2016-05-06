cd469e351a4ec9321d9e5ddfe26c623f481f128d
READING <9505180@graniteschools.org>
2016-05-06 12:45:11 -0600

    Added a way to retrieve mp3 metadata

--- src/com/chrisreading/musicplayer/model/Song.java
+++ src/com/chrisreading/musicplayer/model/Song.java
@@ -2,6 +2,8 @@ package com.chrisreading.musicplayer.model;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.MapChangeListener;
+import javafx.collections.MapChangeListener.Change;
 import javafx.scene.media.Media;
 
 /**
@@ -11,6 +13,9 @@ public class Song {
 	
 	/** Location of the music file */
 	private StringProperty file;
+	
+	/** Song Metadata */
+	private StringProperty title, artist, album, length;
 	
 	/** Location turned to media */
 	private Media media;
@@ -22,6 +27,31 @@ public class Song {
 	public Song(String file) {
 		this.file = new SimpleStringProperty(file);
 		media = new Media(this.file.get());
+		
+		// retrieve the mp3 metadata
+		media.getMetadata().addListener(new MapChangeListener<String, Object>() {
+			@Override
+			public void onChanged(Change<? extends String, ? extends Object> ch) {
+				if(ch.wasAdded()) {
+					handleMetaData(ch.getKey(), ch.getValueAdded());
+				}
+			}
+		});
+	}
+	
+	/**
+	 * Grabs the Mp3's metadata values
+	 */
+	private void handleMetaData(String key, Object value) {
+		if(key.equals("album"))
+			album = new SimpleStringProperty(value.toString());
+		else if(key.equals("artist"))
+			artist = new SimpleStringProperty(value.toString());
+		if(key.equals("title"))
+			title = new SimpleStringProperty(value.toString());
+		if(key.equals("length"))
+			length = new SimpleStringProperty(value.toString());
+		
 	}
 	
 	/** GETTER & SETTERS */
@@ -36,6 +66,38 @@ public class Song {
 	
 	public Media getMedia() {
 		return media;
+	}
+	
+	public StringProperty getTitleProperty() {
+		return title;
+	}
+	
+	public String getTitle() {
+		return title.get();
+	}
+	
+	public StringProperty getAlbumProperty() {
+		return album;
+	}
+	
+	public String getAlbum() {
+		return album.get();
+	}
+	
+	public StringProperty getArtistProperty() {
+		return artist;
+	}
+	
+	public String getArtist() {
+		return artist.get();
+	}
+	
+	public StringProperty getLengthProperty() {
+		return length;
+	}
+	
+	public String getLength() {
+		return length.get();
 	}
 
 }
