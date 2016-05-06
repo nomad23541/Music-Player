0d2c0387e899221ed3f8fc113973a3fdac5bc41d
READING <9505180@graniteschools.org>
2016-05-06 12:16:41 -0600

    PlayerOverview now shows inside of the RootLayout

--- src/com/chrisreading/musicplayer/MainApp.java
+++ src/com/chrisreading/musicplayer/MainApp.java
@@ -1,6 +1,12 @@
 package com.chrisreading.musicplayer;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -9,11 +15,51 @@ import javafx.stage.Stage;
 public class MainApp extends Application {
 	
 	private Stage primaryStage;
+	private BorderPane rootLayout; // layout of the root 
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("Music Player");
+		
+		initRootLayout();
+		showPlayerOverview();
+	}
+	
+	/**
+	 * Initializes the root layout
+	 */
+	public void initRootLayout() {
+		try {
+			// load root layout from file
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			// show the scene containing rootlayout
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Shows the player overview inside of the rootlayout
+	 */
+	public void showPlayerOverview() {
+		try {
+			// load to do overview
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/PlayerOverview.fxml"));
+			AnchorPane todoOverview = (AnchorPane) loader.load();
+			
+			// set person overview to center of root layout
+			rootLayout.setCenter(todoOverview);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/**
